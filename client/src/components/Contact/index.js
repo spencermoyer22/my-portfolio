efdf6573d02f08e33649d512e743b4dbb77634e7
Big Head <spencer.moyer16@yahoo.com>
2021-06-21 01:51:26 -0700

    added contact form

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -6,9 +6,57 @@ function Contact() {
     const { name, email, message } = formState;
     const [errorMessage, setErrorMessage]= useState('');
 
-    return (
-        <div>
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            }
+             else {
+                 setErrorMessage('');
+             }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value});
+        }
+    }
 
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
+    return (
+        <div className='container'>
+            <h2 className='mt-3'>Contact</h2>
+            <div className='row justify-content-center'>
+                <form onSubmit={handleSubmit} className='d-inline col-6'>
+                    <div>
+                        <label htmlFor='name' className='form-label'>Name:</label>
+                        <input className='form-control' type='text' name='name' defaultValue={name} onBlur={handleChange} />
+                    </div>
+                    <div>
+                        <label htmlFor='email' className='form-label'>Email:</label>
+                        <input className='form-control' type='email' name='email' defaultValue={email} onBlur={handleChange} />
+                    </div>
+                    <div>
+                        <label htmlFor='message' className='form-label'>Message:</label>
+                        <textarea className='form-control' name='message' rows='5' defaultValue={message} onBlur={handleChange} />
+                    </div>
+                    {errorMessage && (
+                        <div>
+                            <p class='text-danger'>{errorMessage}</p>
+                        </div>
+                    )}
+                    <button type='submit' className='btn'>Submit</button>
+                </form>
+            </div>
         </div>
     );
 };
