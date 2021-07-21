8951cab4722b3fad665b5bd1aa4bd1924639216f
Big Head <spencer.moyer16@yahoo.com>
2021-07-21 13:42:41 -0700

    minor adjustments to styling of footer and about

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -11,12 +11,8 @@ function About() {
                 ></img>
                 </div>
                 <div style={{fontSize: 20}} className='text-start ml-5 col-lg-6 col-sm-12'>
-                    <h2 className='mb-4'>About Me</h2>
                     <p className='mb-4'>
-                        Hello, I'm Spencer! I am a Full-Stack Developer who loves learning and challenging myself daily. I have most recently been developing my skills in the MERN stack.
-                    </p>
-                    <p>
-                        When I'm not studying web development I am usually out somewhere being active. Whether it involves sports, hiking, or just going on walks around the beach I love getting up and moving!
+                        Hello, I'm Spencer! I am a Full-Stack Developer who loves learning and challenging myself daily. I have most recently been developing my skills in the MERN stack. I have received my Full Stack Wev Development Certificate from the University of Arizona's bootcamp program. Before that I finished a Bachelor's degree in Computer Information Systems with a concentration in software development as well. Programming has been a passion of mine for the last few years, and I am excited to keep learning more. When I'm not studying web development I am usually out somewhere being active. Whether it involves sports, hiking, or just going on walks around the beach I love getting up and moving!
                     </p>
                 </div>
             </div>
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,20 +1,20 @@
 function Footer() {
     return (
-        <footer className='mt-5 text-center py-3'>
+        <footer className='m-5 text-center py-3 d-flex justify-content-center align-items-center'>
+             <p className='mt-3'>
+                &copy;2021 Spencer Moyer
+            </p>
             <ul style={{listStyle: 'none'}} className='d-flex justify-content-center'>
                 <li className='mx-3'>
-                    <a href="https://www.linkedin.com/in/spencer-moyer-08b347133/" target="_blank"><img src={require("../../assets/images/linkedin-logo.png").default} alt="LinkedIn logo" style={{height: 50}}/></a>
+                    <a href="https://www.linkedin.com/in/spencer-moyer-08b347133/" target="_blank" rel='noreferrer'><img src={require("../../assets/images/linkedin-logo.png").default} alt="LinkedIn logo" style={{height: 30}}/></a>
                 </li>
                 <li className='mx-3'>
-                    <a href="https://twitter.com/spencer_moyer22" target="_blank"><img src={require('../../assets/images/twitter-logo.png').default} alt="Twitter logo" style={{height: 50}}/></a>
+                    <a href="https://twitter.com/spencer_moyer22" target="_blank" rel='noreferrer'><img src={require('../../assets/images/twitter-logo.png').default} alt="Twitter logo" style={{height: 30}}/></a>
                 </li>
                 <li className='mx-3'>
-                    <a href="https://github.com/spencermoyer22" target="_blank"><img src={require('../../assets/images/github-logo.png').default} alt="GitHub logo" style={{height: 50}}/></a>
+                    <a href="https://github.com/spencermoyer22" target="_blank" rel='noreferrer'><img src={require('../../assets/images/github-logo.png').default} alt="GitHub logo" style={{height: 30}}/></a>
                 </li>
             </ul>
-            <p className='mt-3'>
-                &copy;2021 Spencer Moyer | spencer.moyer16@email.com
-            </p>
         </footer>
     );
 };
