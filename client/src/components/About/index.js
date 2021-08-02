cb3f02381d9f83a85540e160cbfe342e5b2b37a2
Big Head <spencer.moyer16@yahoo.com>
2021-08-02 14:04:12 -0700

    added color to background and font and adjusted about me section

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,11 +1,25 @@
 function About() {
     return (
         <div className='d-flex justify-content-center mt-5 px-5'>
-            <div className=''>
-                <div style={{fontSize: 20}} className=''>
+            <div className='mt-5'>
+                <div style={{fontSize: 20}} className='' id='about-text'>
                     <h2 className='text-center'>Hello, my name is Spencer Moyer</h2>
                     <p className='my-4 text-start px-5'>
-                        I am a Full-Stack Developer who loves learning and challenging myself daily. I have most recently been developing my skills in the MERN stack. I have received my Full Stack Wev Development Certificate from the University of Arizona's bootcamp program. Before that I finished a Bachelor's degree in Computer Information Systems with a concentration in software development as well. Programming has been a passion of mine for the last few years, and I am excited to keep learning more. When I'm not studying web development I am usually out somewhere being active. Whether it involves sports, hiking, or just going on walks around the beach I love getting up and moving!
+                        I am a Full-Stack Developer based in San Diego, California. I recently earned a Full Stack Web Development certificate through University of Arizona. I also graduated from Tiffin University with a Bachelor's in Computer Information Systems. I am an enthusiastic developer looking to grow my skills as a young programmer.
+                    </p>
+                    <p className='my-4 text-start px-5'>
+                        Some other things I enjoy when I am not coding
+                        <ul className='mt-3'>
+                            <li>
+                                Working Out
+                            </li>
+                            <li>
+                                Playing video games
+                            </li>
+                            <li>
+                                Traveling
+                            </li>
+                        </ul>
                     </p>
                 </div>
                 <div>
