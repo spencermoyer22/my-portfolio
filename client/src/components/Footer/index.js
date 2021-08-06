e7251a14ca94e6a402650edfc17f8474828737e8
Big Head <spencer.moyer16@yahoo.com>
2021-08-06 15:59:10 -0700

    finished first edit of contact page

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,21 +1,21 @@
 function Footer() {
     return (
         <footer className='mt-5 pt-3 d-flex flex-wrap justify-content-center align-items-center'>
-             <p>
+            <p>
                 Copyright &copy; 2021 Spencer Moyer
             </p>
-            <ul style={{listStyle: 'none'}} className='d-flex justify-content-center px-4'>
+            <ul style={{ listStyle: 'none' }} className='d-flex justify-content-center px-4'>
                 <li className='mx-3'>
-                    <a href="https://www.linkedin.com/in/spencer-moyer-08b347133/" className='grow' target="_blank" rel='noreferrer'><img src={require("../../assets/images/linkedin-logo.png").default} alt="LinkedIn logo" style={{height: 25}}/></a>
+                    <a href="https://www.linkedin.com/in/spencer-moyer-08b347133/" className='grow' target="_blank" rel='noreferrer'><img src={require("../../assets/images/linkedin-logo.png").default} alt="LinkedIn logo" style={{ height: 25 }} /></a>
                 </li>
                 <li className='mx-3'>
-                    <a href="https://twitter.com/spencer_moyer22" className='grow' target="_blank" rel='noreferrer'><img src={require('../../assets/images/twitter-logo.png').default} alt="Twitter logo" style={{height: 25}}/></a>
+                    <a href="https://twitter.com/spencer_moyer22" className='grow' target="_blank" rel='noreferrer'><img src={require('../../assets/images/twitter-logo.png').default} alt="Twitter logo" style={{ height: 25 }} /></a>
                 </li>
                 <li className='mx-3'>
-                    <a href="https://github.com/spencermoyer22" className='grow' target="_blank" rel='noreferrer'><img src={require('../../assets/images/github-logo.png').default} alt="GitHub logo" style={{height: 25}}/></a>
+                    <a href="https://github.com/spencermoyer22" className='grow' target="_blank" rel='noreferrer'><img src={require('../../assets/images/github-logo.png').default} alt="GitHub logo" style={{ height: 25 }} /></a>
                 </li>
                 <li className='mx-3'>
-                    <a href="mailto:spencer.moyer16@yahoo.com" className='grow'><img src={require('../../assets/images/email.png').default} alt="Email logo" style={{height: 25}}/></a>
+                    <a href="mailto:spencer.moyer16@yahoo.com" className='grow'><img src={require('../../assets/images/email.png').default} alt="Email logo" style={{ height: 25 }} /></a>
                 </li>
             </ul>
         </footer>
