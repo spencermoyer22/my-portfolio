1652188e228c389849ddc4289cd95c76a7290831
Big Head <spencer.moyer16@yahoo.com>
2021-08-09 12:47:43 -0700

    fixed responsiveness in project overlay

--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -1,6 +1,6 @@
 function Projects() {
     const projects = [
-        { name: 'tutor-time', description: 'Full Stack MERN app that allows students to find tutors based on subject. Bcrypt and JSON Web Tokens verify users and information. Apollo Client/Server and GraphQL are used to write and test queries and mutations. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/tutor-time', website: 'https://tranquil-hollows-19559.herokuapp.com/' }, 
+        { name: 'tutor-time', description: 'Full Stack MERN find tutors based on subject. Bcrypt and JSON Web Tokens verify users and information. Apollo Client/Server and GraphQL are used to write and test queries and mutations. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/tutor-time', website: 'https://tranquil-hollows-19559.herokuapp.com/' }, 
         { name: 'portfolio', description: 'Front end React app created to build myself a portfolio site. Used React, JSX, and CSS to create display and animations. Deployed to GitHub pages.', github: 'https://github.com/spencermoyer22/portfolio', website: 'https://spencermoyer22.github.io/portfolio/' }, 
         { name: 'shop-shop', description: 'Full Stack MERN e-commerce site. Converted the project from prop drilling to global state management using Redux. Implemented a test Stripe program for payment method. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/shop-shop', website: 'https://sheltered-dusk-84272.herokuapp.com/' }, 
         { name: 'gun-show', description: ' Health based social media account using MVC architecture. Handlebars create the fornt end. MySQL and Sequelize make up the back end. Uses express to create API routes. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/JustTechGunShow', website: 'https://thawing-plains-49978.herokuapp.com/' }, 
@@ -20,7 +20,7 @@ function Projects() {
                             alt={project.name}
                             className='mx-auto project-img'
                         ></img>
-                        <div className='project-overlay py-4'>
+                        <div className='project-overlay py-2 py-md-4'>
                             <p style={{fontSize: 16}} className='text-start px-4'>{project.description}</p>
                             <div className='d-flex justify-content-around'>
                                 <button className='btn-contact btn-project'><a href={project.github} target='_blank' rel='noreferrer'>GitHub</a></button>
