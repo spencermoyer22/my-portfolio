0e03ff3ebb08b1455eae88eb371aaee0a8f635e4
Big Head <spencer.moyer16@yahoo.com>
2021-08-08 21:36:52 -0700

    added overlay and some transition to project images

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,18 +1,22 @@
 function Portfolio() {
-    const projects = [{name: 'tutor-time'}, {name: 'gun-show'}, {name: 'event-finder'}, {name: 'run-buddy'}, {name: 'random1'}, {name: 'random2'}];
+    const projects = [{ name: 'tutor-time' }, { name: 'gun-show' }, { name: 'event-finder' }, { name: 'run-buddy' }, { name: 'random1' }, { name: 'random2' }];
 
     return (
-        <div className='container text-center'>
+        <div className='container text-center mx-0 px-4'>
             <h2 className='mt-5'>Recent Work</h2>
-            <p style={{fontSize: 20}}>Here are some projects I have worked on recently</p>
+            <p style={{ fontSize: 20 }}>Here are some projects I have worked on recently</p>
             <div className='row mt-5'>
                 {projects.map((project) => (
-                    <img 
-                    src={require(`../../assets/images/${project.name}.png`).default}
-                    style={{height: 300}}
-                    alt={project.name}
-                    className='mx-auto col-lg-4 col-md-6 col-sm-12 my-2'
-                ></img>
+                    <div className='project mx-auto col-xl-4 col-lg-6 col-sm-12 p-0 m-0' style={{position: 'relative'}}>
+                        <img
+                            src={require(`../../assets/images/${project.name}.png`).default}
+                            alt={project.name}
+                            className='mx-auto project-img'
+                        ></img>
+                        <div className='project-overlay'>
+                            <p>{project.name}</p>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
