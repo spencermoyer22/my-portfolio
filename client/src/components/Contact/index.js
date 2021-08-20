0019091ee48accc72afeb8ebc13d526ff107a86d
Big Head <spencer.moyer16@yahoo.com>
2021-08-19 18:00:23 -0700

    fixed spelling and button links

--- client/src/components/Contact/index.js
+++ client/src/components/Contact/index.js
@@ -3,15 +3,15 @@ function Contact() {
     return (
         <div className='container mt-5'>
             <div className='row d-flex justify-content-center'>
-                <div className='col-12 col-xl-5 contact-section px-4 pt-4 pt-xl-5'>
+                <div className='col-12 col-xl-5 contact-section px-4 py-4 pt-xl-5'>
                     <h2 style={{ fontSize: 60 }}><span className='tertiary'>Contact</span> me</h2>
                     <p style={{ fontSize: 22 }} className='mt-3 mt-xlg-5'>If you are interested in my work or just want to chat I encourage you to reach out. You can email me at the link below.</p>
                     <a href="mailto:spencer.moyer16@yahoo.com" className='text-decoration-none tertiary middle' style={{ fontSize: 25 }}>spencer.moyer16@gmail.com</a>
                     <h2 style={{ fontSize: 60 }} className='mt-4 mt-xxl-5'><span className='tertiary'>Follow</span> me</h2>
-                    <p style={{ fontSize: 22 }} className='mt-5'>Follow me on my LinkedIn, Twitter, and Github at the links below.</p>
-                    <button className='px-5 py-2 btn-contact'><a href="https://www.linkedin.com/in/spencer-moyer-08b347133/" target="_blank" rel='noreferrer'><img src={require("../../assets/images/linkedin-logo.png").default} alt="LinkedIn logo" style={{ height: 25 }} /></a></button>
-                    <button className='px-5 py-2 btn-contact'><a href="https://twitter.com/spencer_moyer22" target="_blank" rel='noreferrer'><img src={require('../../assets/images/twitter-logo.png').default} alt="Twitter logo" style={{ height: 25 }} /></a></button>
-                    <button className='px-5 py-2 btn-contact'><a href="https://github.com/spencermoyer22" target="_blank" rel='noreferrer'><img src={require('../../assets/images/github-logo.png').default} alt="GitHub logo" style={{ height: 25 }} /></a></button>
+                    <p style={{ fontSize: 22 }} className='my-5'>Follow me on my LinkedIn, Twitter, and Github at the links below.</p>
+                    <a className='btn-contact btn-project text-decoration-none px-5 py-3' href="https://www.linkedin.com/in/spencer-moyer-08b347133/" target="_blank" rel='noreferrer'><img src={require("../../assets/images/linkedin-logo.png").default} alt="LinkedIn logo" style={{ height: 25 }} /></a>
+                    <a className='btn-contact btn-project text-decoration-none px-5 py-3' href="https://twitter.com/spencer_moyer22" target="_blank" rel='noreferrer'><img src={require('../../assets/images/twitter-logo.png').default} alt="Twitter logo" style={{ height: 25 }} /></a>
+                    <a className='btn-contact btn-project text-decoration-none px-5 py-3' href="https://github.com/spencermoyer22" target="_blank" rel='noreferrer'><img src={require('../../assets/images/github-logo.png').default} alt="GitHub logo" style={{ height: 25 }} /></a>
                 </div>
                 <div className='col-5 px-0 d-none d-xl-inline'>
                     <img src={require('../../assets/images/selfie.jpeg').default} alt='me' className='w-100' />
--- client/src/components/Projects/index.js
+++ client/src/components/Projects/index.js
@@ -3,8 +3,8 @@ function Projects() {
         { name: 'tutor-time', description: 'Full Stack MERN site to find tutors based on subject. Bcrypt and JSON Web Tokens verify users and information. Apollo Client/Server and GraphQL are used to write and test queries and mutations. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/tutor-time', website: 'https://tranquil-hollows-19559.herokuapp.com/' }, 
         { name: 'portfolio', description: 'Front end React app created to build myself a portfolio site. Used React, JSX, and CSS to create display and animations. Deployed to GitHub pages.', github: 'https://github.com/spencermoyer22/portfolio', website: 'https://spencermoyer22.github.io/portfolio/' }, 
         { name: 'shop-shop', description: 'Full Stack MERN e-commerce site. Converted the project from prop drilling to global state management using Redux. Implemented a test Stripe program for payment method. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/shop-shop', website: 'https://sheltered-dusk-84272.herokuapp.com/' }, 
-        { name: 'gun-show', description: ' Health based social media account using MVC architecture. Handlebars create the fornt end. MySQL and Sequelize make up the back end. Uses express to create API routes. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/JustTechGunShow', website: 'https://thawing-plains-49978.herokuapp.com/' }, 
-        { name: 'budget-tracker', description: 'Created a Progressive Web Application to track a budget. Uses IndexedDb and Service Workers to function offline and save information to MongoDB when back online. Uses manifest to downloaded site as an application. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/budget-tracker', website: 'https://obscure-journey-56604.herokuapp.com/' }, 
+        { name: 'gun-show', description: ' Health based social media account using MVC architecture. Handlebars create the front end. MySQL and Sequelize make up the back end. Uses express to create API routes. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/JustTechGunShow', website: 'https://thawing-plains-49978.herokuapp.com/' }, 
+        { name: 'budget-tracker', description: 'Created a Progressive Web Application to track a budget. Uses IndexedDb and Service Workers to function offline and save information to MongoDB when back online. Uses manifest to download site as an application. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/budget-tracker', website: 'https://obscure-journey-56604.herokuapp.com/' }, 
         { name: 'note-taker', description: 'Simple note taking app using Express, Node.js and JavaScript to save notes and perform CRUD operations. Deployed to Heroku.', github: 'https://github.com/spencermoyer22/note-taker', website: 'https://rocky-springs-14001.herokuapp.com/notes' }
     ];
 
@@ -23,8 +23,8 @@ function Projects() {
                         <div className='project-overlay py-2 py-md-4'>
                             <p style={{fontSize: 16}} className='text-start px-4'>{project.description}</p>
                             <div className='d-flex justify-content-around'>
-                                <button className='btn-contact btn-project'><a href={project.github} target='_blank' rel='noreferrer'>GitHub</a></button>
-                                <button className='btn-contact btn-project'><a href={project.website} target='_blank' rel='noreferrer'>Website</a></button>
+                                <a className='btn-contact btn-project text-decoration-none px-2' href={project.github} target='_blank' rel='noreferrer'>GitHub</a>
+                                <a className='btn-contact btn-project text-decoration-none px-2' href={project.website} target='_blank' rel='noreferrer'>Website</a>
                             </div>
                         </div>
                     </div>
